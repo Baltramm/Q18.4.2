f1a76df794328f2a8320b20ef341d2a91cb19889
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-08-11 03:01:52 +0600

    Добавьте файлы проекта.

---
+++ Q18.4.2/Command.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q18._4._2
+{
+    public abstract class Command
+    {
+        public abstract Task Run(string arg);
+    }
+}
---
+++ Q18.4.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using YoutubeExplode;
+using System.Threading.Tasks;
+
+namespace Q18._4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
